6e2424b834d63d31f3ea96824584840026235f7d
Elshan's Projects <89624267+Elshan-Projects@users.noreply.github.com>
2021-12-01 14:54:39 +0400

    Fixed all the bugs

--- FLIGHT_SCHEDULING_ELSHAN.py
+++ FLIGHT_SCHEDULING_ELSHAN.py
@@ -40,17 +40,28 @@ def schedule(n_a,n_c,time_in_days,cities):
     th = time_in_days*24
     records=[]
     stage = 0
+    x=1
+    y=2
     for  j in range(n_c):
+
         for jj in range(n_a):
-            records.append( (j, jj, stage+jj*0.25, stage+jj*0.25+ds[j], stage+jj*0.25+2*ds[j]) )
+            records.append( (j, jj, x, stage+jj*0.25, stage+jj*0.25+ds[j], y, stage+jj*0.25+2*ds[j]) )
             if(records[-1][-1]>=th):
                 break
         stage=records[-1][-1]
+        x, y = y, x
 
     table = []
     for m in range(len(records)):
-        table.append(( cl[ records[m][0] ], "Plane_"+str( records[m][1] ), "Leaves at "+time_converter( records[m][2] ), 
-                     "Arrives at "+time_converter( records[m][3] ) , "Returns at "+time_converter( records[m][4] ) ))
+        table.append(( 
+            cl[ records[m][0] ], 
+            "Plane_"+str( records[m][1] ), 
+            " Landed at "+str(records[m][2]), 
+            " Leaves at "+time_converter( records[m][3] ), 
+            " Arrives at "+time_converter( records[m][4] ) ,
+            " Landed at "+str(records[m][5]), 
+            " Returns at "+time_converter( records[m][6] ) 
+            ))
         
     return table
 
@@ -60,4 +71,3 @@ for b in range(len(final_result)):
     print(final_result[b])
     if ( ((b+1)%n_a)==0 ):
         print("\n")
-
